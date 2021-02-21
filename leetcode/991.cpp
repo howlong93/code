f12d34c4e5b9d87fc6bd020cad60f19695c97732
howlong93 <brian20040618@gmail.com>
2021-02-21 22:29:27 +0800

    Broken Calculator

---
+++ leetcode/991.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        if (X >= Y) return X-Y;
+        if (Y % 2) return 1+brokenCalc (X, Y+1);
+        else return 1+brokenCalc (X, Y/2);
+    }
+};
