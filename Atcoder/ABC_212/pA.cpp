a7a8a3009ab44e8c99ae9eca839dec61124a0ab0
brian <69999779+howlong93@users.noreply.github.com>
2021-08-06 15:51:38 +0800

    Atcoder contest

---
+++ Atcoder/ABC_212/pA.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	
+	if (a == 0) cout << "Silver\n";
+	else if (b == 0) cout << "Gold\n";
+	else cout << "Alloy\n";
+	
+	return 0;
+}
