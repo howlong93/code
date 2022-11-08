31c5eace983ad4b2109cde6c1d68bca3fca1151a
brian <69999779+howlong93@users.noreply.github.com>
2022-11-08 17:12:45 +0800

    Brute Force

--- Atcoder/ABC_275/C.cpp
+++ Atcoder/ABC_275/C.cpp
@@ -12,12 +12,12 @@ bool isNode (int x, int y) {
 
 	return false;
 }
-set<set<pair<int, int>>> cnt;
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(false);
 
+	set<set<pair<int, int>>> cnt;
 	for (int i = 0; i < 9; i++)
 		cin >> rec[i];
 
