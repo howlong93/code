7235aae9e81183b69f8e30bccd5ac95878a7ff08
brian20040618@gmail.com <brian20040618@gmail.com>
2023-01-29 10:56:33 +0800

    EE_Camp_2023

--- school/university/EE_Camp/WASD_version.cpp
+++ school/university/EE_Camp/WASD_version.cpp
@@ -8,7 +8,8 @@ using namespace std;
 #define BOMB_NUM 10
 
 void CleanScreen();
-void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int, int);
+char get_choice();
+void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int, int, int, int);
 int check (int graph[SIZE][SIZE], int, int);
 int bomb_in_area (int graph[SIZE][SIZE], int, int);
 
@@ -20,7 +21,9 @@ void CleanScreen() {
 #endif
 }
 
-void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int mined_cnt, int flag_cnt) {
+void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int mined_cnt, int flag_cnt,
+			 int cursor_row, int cursor_col) {
+
 	cout << "\033[1;32;45m   ";
 	for (int i = 0; i < SIZE; i++) cout << i << ' ';
 	cout << "\033[0m\n";
@@ -28,12 +31,16 @@ void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int mined_cnt, int flag
 	for (int i = 0; i < SIZE; i++) {
 		cout << "\033[1;32;45m " << i << "\033[0m ";
 		for (int j = 0; j < SIZE; j++) {
-			if (vis[i][j] == 0) cout << "- ";
+			if (i == cursor_row && j == cursor_col) cout << "\033[5;44m"; //閃爍
+
+			if (vis[i][j] == 0) cout << "-\033[0m ";
 			else if (vis[i][j] == 1) {
 				if (graph[i][j] == -1) cout << "\033[1;31m*\033[0m ";
-				else cout << graph[i][j] << ' ';
+				else cout << graph[i][j] << "\033[0m ";
 			}
 			else if (vis[i][j] == 2) cout << "\033[1;36m$\033[0m ";
+
+			cout << "\033[0m"; //恢復
 		}
 		cout << '\n';
 	}
@@ -47,10 +54,10 @@ void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int mined_cnt, int flag
 }
 
 int check (int graph[SIZE][SIZE], int row, int col) {
-	if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) return 0; //invalid
-	if (graph[row][col] == -1) return 1;	//bomb
-	if (graph[row][col] == 0) return 2;		//0
-	else return 3;							//other
+	if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) return 0;
+	if (graph[row][col] == -1) return 1;
+	if (graph[row][col] == 0) return 2;
+	else return 3;
 }
 
 int bomb_in_area (int graph[SIZE][SIZE], int row, int col) {
@@ -69,7 +76,9 @@ int main() {
 	srand(time(NULL));
 
 	//build graph
-	int graph[SIZE][SIZE] = {0}, vis[SIZE][SIZE] = {0}, mined_cnt = 0, flag_cnt = 0;
+	int graph[SIZE][SIZE] = {0}, vis[SIZE][SIZE] = {0};
+	int mined_cnt = 0, flag_cnt = 0;
+	int cursor_row = 0, cursor_col = 0;
 
 	for (int i = 0; i < BOMB_NUM; i++) {
 		int pos = (int) ((double) rand()/RAND_MAX * SIZE*SIZE);
@@ -85,36 +94,35 @@ int main() {
 				graph[i][j] = bomb_in_area (graph, i, j);
 		}
 	}
-
+	//graph built
+	
 	CleanScreen();
-	output(graph, vis, mined_cnt, flag_cnt);
+	output(graph, vis, mined_cnt, flag_cnt, cursor_row, cursor_col);
 
 	bool fail = false;
 	while (mined_cnt < SIZE*SIZE - BOMB_NUM) {
-		bool on_visited = false;
-		int ch, row, col;
-		cin >> ch >> row >> col;
+		char ch = '\0';
 
 		CleanScreen();
-		if (check (graph, row, col) == 0) {
-			output (graph, vis, mined_cnt, flag_cnt);
-			cout << "\033[1;31mout of range!!\033[0m\n";
-			continue;
-		}
+		output(graph, vis, mined_cnt, flag_cnt, cursor_row, cursor_col);
+		while (ch == '\0') ch = getchar();
 
-		if (ch == 0) {
-			if (vis[row][col])on_visited = true; 
-			else if (graph[row][col] == -1) { //stepped on bomb
+		if ((ch == 'w' || ch == 80) && check (graph, cursor_row-1, cursor_col)) cursor_row--;
+		else if ((ch == 's' || ch == 75) && check (graph, cursor_row+1, cursor_col)) cursor_row++;
+		else if ((ch == 'a' || ch == 77) && check (graph, cursor_row, cursor_col-1)) cursor_col--;
+		else if ((ch == 'd' || ch == 72) && check (graph, cursor_row, cursor_col+1)) cursor_col++;
+		else if(ch == '0') {
+			if (graph[cursor_row][cursor_col] == -1) { //stepped on bomb
 				for (int t = 0; t < SIZE*SIZE; t++) vis[t/SIZE][t%SIZE] = 1;
 				fail = true;
 			}
-			else if (graph[row][col] > 0) {
-				vis[row][col] = 1;
+			else if (graph[cursor_row][cursor_col] > 0) {
+				vis[cursor_row][cursor_col] = 1;
 				mined_cnt++;
 			}
 			else {
 				queue<int> save;
-				save.push (row*SIZE + col);
+				save.push (cursor_row*SIZE + cursor_col);
 
 				while (save.size()) {
 					int cur = save.front();
@@ -123,6 +131,8 @@ int main() {
 
 					if (vis[cur_row][cur_col]) continue;
 
+					cursor_row = cur_row;
+					cursor_col = cur_col;
 					vis[cur_row][cur_col] = 1;
 					mined_cnt++;
 	
@@ -138,23 +148,21 @@ int main() {
 				}
 			}
 		}
-		else if (ch == 1) {
-			if (vis[row][col] == 1) on_visited = true;
-			else if (vis[row][col] == 0) vis[row][col] = 2, flag_cnt++;
-			else if (vis[row][col] == 2) vis[row][col] = 0, flag_cnt--;
+		else if (ch == '1') {
+			if (vis[cursor_row][cursor_col] == 0) vis[cursor_row][cursor_col] = 2, flag_cnt++;
+			else if (vis[cursor_row][cursor_col] == 2) vis[cursor_row][cursor_col] = 0, flag_cnt--;
 		}
 
-		output (graph, vis, mined_cnt, flag_cnt);
-
-		//special cases : visited, lose
-		if (on_visited) cout << "\033[1;33mVertex visited!!\033[0m\n";
+		CleanScreen();
+		output (graph, vis, mined_cnt, flag_cnt, cursor_row, cursor_col);
+		//special cases : lose
 		if (fail) {
 			cout << "\033[1;5;31mStepped on bomb\033[0m\n";
 			break;
 		}
 	}
 
-	if (!fail) cout << "\033[5;32mYou win!!!\033[0m\n";
+	if (!fail) cout << "\033[1;5;32mYou win!!!\033[0m\n";
 
 	return 0;
 }
---
+++ school/university/EE_Camp/final_version.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <cstdlib>
+#include <queue>
+#include <unistd.h>
+#include <assert.h>
+#include <termios.h>
+#include <string.h>
+
+using namespace std;
+
+#define SIZE 8
+#define BOMB_NUM 10
+
+void CleanScreen();
+char get_choice();
+void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int, int, int, int);
+int check (int graph[SIZE][SIZE], int, int);
+int bomb_in_area (int graph[SIZE][SIZE], int, int);
+
+void CleanScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+char get_choice(void) {
+    int c = 0;
+    struct termios org_opts, new_opts;
+    int res = 0;
+    /*---- store old settings ----*/
+    res = tcgetattr(STDIN_FILENO, &org_opts);
+    assert(res == 0);
+    /*---- set new terminal parms ----*/
+    memcpy(&new_opts, &org_opts, sizeof(new_opts));
+    new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
+    c = getchar();
+    /*---- restore old settings ----*/
+    res = tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+    assert(res == 0);
+    return c;
+}
+
+void output (int graph[SIZE][SIZE], int vis[SIZE][SIZE], int mined_cnt, int flag_cnt,
+			 int cursor_row, int cursor_col) {
+
+	cout << "\033[1;32;45m   ";
+	for (int i = 0; i < SIZE; i++) cout << i << ' ';
+	cout << "\033[0m\n";
+
+	for (int i = 0; i < SIZE; i++) {
+		cout << "\033[1;32;45m " << i << "\033[0m ";
+		for (int j = 0; j < SIZE; j++) {
+			if (i == cursor_row && j == cursor_col) cout << "\033[5;44m"; //閃爍
+
+			if (vis[i][j] == 0) cout << "-\033[0m ";
+			else if (vis[i][j] == 1) {
+				if (graph[i][j] == -1) cout << "\033[1;31m*\033[0m ";
+				else cout << graph[i][j] << "\033[0m ";
+			}
+			else if (vis[i][j] == 2) cout << "\033[1;36m$\033[0m ";
+
+			cout << "\033[0m"; //恢復
+		}
+		cout << '\n';
+	}
+
+	cout << "\033[1;33m\n"
+         << "mined places: " << mined_cnt << " /" << SIZE*SIZE - BOMB_NUM << '\t'
+         << "flagged bombs: " << flag_cnt << " /" << BOMB_NUM
+	     << "\033[0m\n";
+
+	return;
+}
+
+int check (int graph[SIZE][SIZE], int row, int col) {
+	if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) return 0;
+	if (graph[row][col] == -1) return 1;
+	if (graph[row][col] == 0) return 2;
+	else return 3;
+}
+
+int bomb_in_area (int graph[SIZE][SIZE], int row, int col) {
+	int bomb_num = 0;
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) 
+			if (i == 0 && j == 0) continue;
+			if (check (graph, row+i, col+j) == 1)
+				bomb_num++;
+		}
+	}
+	return bomb_num;
+}
+
+int main() {
+	srand(time(NULL));
+
+	//build graph
+	int graph[SIZE][SIZE] = {0}, vis[SIZE][SIZE] = {0};
+	int mined_cnt = 0, flag_cnt = 0;
+	int cursor_row = 0, cursor_col = 0;
+
+	for (int i = 0; i < BOMB_NUM; i++) {
+		int pos = (int) ((double) rand()/RAND_MAX * SIZE*SIZE);
+		while (graph[pos/SIZE][pos%SIZE] == -1) 
+			pos = (int) ((double) rand()/RAND_MAX * SIZE*SIZE);
+		
+		graph[pos/SIZE][pos%SIZE] = -1;
+	}
+
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (graph[i][j] == 0)
+				graph[i][j] = bomb_in_area (graph, i, j);
+		}
+	}
+	//graph built
+	
+	CleanScreen();
+	output(graph, vis, mined_cnt, flag_cnt, cursor_row, cursor_col);
+
+	bool fail = false;
+	while (mined_cnt < SIZE*SIZE - BOMB_NUM) {
+		char ch = '\0';
+
+		CleanScreen();
+		output(graph, vis, mined_cnt, flag_cnt, cursor_row, cursor_col);
+		while (ch == '\0') ch = get_choice();
+
+		if ((ch == 'w' || ch == 65) && check (graph, cursor_row-1, cursor_col)) cursor_row--;
+		else if ((ch == 's' || ch == 66) && check (graph, cursor_row+1, cursor_col)) cursor_row++;
+		else if ((ch == 'a' || ch == 68) && check (graph, cursor_row, cursor_col-1)) cursor_col--;
+		else if ((ch == 'd' || ch == 67) && check (graph, cursor_row, cursor_col+1)) cursor_col++;
+		else if (ch == 'j') { //mine
+			if (vis[cursor_row][cursor_col] == 1) continue;
+			else if (graph[cursor_row][cursor_col] == -1) { //stepped on bomb
+				for (int t = 0; t < SIZE*SIZE; t++) vis[t/SIZE][t%SIZE] = 1;
+				fail = true;
+			}
+			else if (graph[cursor_row][cursor_col] > 0) {
+				vis[cursor_row][cursor_col] = 1;
+				mined_cnt++;
+			}
+			else {
+				queue<int> save;
+				save.push (cursor_row*SIZE + cursor_col);
+
+				while (save.size()) {
+					int cur = save.front();
+					int cur_row = cur/SIZE, cur_col = cur%SIZE;
+					save.pop();
+
+					if (vis[cur_row][cur_col]) continue;
+
+					cursor_row = cur_row;
+					cursor_col = cur_col;
+					vis[cur_row][cur_col] = 1;
+					mined_cnt++;
+	
+					if (check (graph, cur_row, cur_col) == 2) {
+						for (int i = -1; i <= 1; i++) {
+							for (int j = -1; j <= 1; j++) {
+								if (i == 0 && j == 0) continue;
+								if (check (graph, cur_row+i, cur_col+j) > 1 && !vis[cur_row+i][cur_col+j])
+									save.push ((cur_row+i) * SIZE + (cur_col+j));
+							}
+						}
+					}
+				}
+			}
+		}
+		else if (ch == 'k') { //flag
+			if (vis[cursor_row][cursor_col] == 0) vis[cursor_row][cursor_col] = 2, flag_cnt++;
+			else if (vis[cursor_row][cursor_col] == 2) vis[cursor_row][cursor_col] = 0, flag_cnt--;
+		}
+
+		CleanScreen();
+		output (graph, vis, mined_cnt, flag_cnt, cursor_row, cursor_col);
+		//special cases : lose
+		if (fail) {
+			cout << "\033[1;5;31mStepped on bomb!!\033[0m\n";
+			break;
+		}
+	}
+
+	if (!fail) cout << "\033[1;5;32mYou win!!!\033[0m\n";
+
+	return 0;
+}
